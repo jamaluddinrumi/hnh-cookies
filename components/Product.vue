7456b2537960194f453eee97c31e7b783e351cd7
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-04-03 00:58:09 +0700

    feat: add products

---
+++ components/Product.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  flipOrder?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="grid grid-cols-2 gap-4">
+    <div :class="flipOrder ? 'order-2' : 'order-1'" class="prose">
+      <h2><slot name="title" /> (Rp<slot name="price" />)</h2>
+      <p>
+        <slot name="description" />
+      </p>
+      <p>
+        <slot name="description2" />
+      </p>
+    </div>
+    <div :class="flipOrder ? 'order-1' : 'order-2'">
+      <slot name="photo" />
+    </div>
+  </div>
+</template>
