36e85961bd97838cf16a755a940b32c2df602fcf
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-04-03 01:29:19 +0700

    seo: add html meta og's

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,13 @@ const props = defineProps<{
 <template>
   <Head>
     <Title> HNH Cookies - Delicious Made Better </Title>
+    <Meta property="og:title" content="HNH Cookies - Delicious Made Better" />
+    <Meta property="og:type" content="food" />
+    <Meta property="og:url" content="https://hnh-cookies.pages.dev/" />
+    <Meta property="og:image" content="/cookies-time.webp" />
+    <Meta property="og:site_name" content="HNH Cookies" />
+    <Meta property="og:locale" content="id_ID" />
+    <Meta property="og:description" content="Cookies Time! Choco Chips Cookies, Baton Cookies, Choctart semua ada di sini! Yuk!" />
   </Head>
   <img src="/articles/hero.jpeg" alt="" />
   <div class="container mx-auto px-4">
