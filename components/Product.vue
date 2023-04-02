f62b369dbf24db6a3e3d23584273ff60eb5c0573
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-04-03 01:13:43 +0700

    style: fix orders for small screen

--- components/Product.vue
+++ components/Product.vue
@@ -5,8 +5,8 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-2 gap-4">
-    <div :class="flipOrder ? 'order-2' : 'order-1'" class="prose">
+  <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 my-8">
+    <div :class="flipOrder ? 'lg:order-2' : 'lg:order-1'" class="prose order-2">
       <h2><slot name="title" /> (Rp<slot name="price" />)</h2>
       <p>
         <slot name="description" />
@@ -15,7 +15,10 @@ const props = defineProps<{
         <slot name="description2" />
       </p>
     </div>
-    <div :class="flipOrder ? 'order-1' : 'order-2'">
+    <div
+      :class="flipOrder ? 'lg:order-1' : 'lg:order-2'"
+      class="order-1 prose prose-img:mb-4 lg:prose-img:mb-0"
+    >
       <slot name="photo" />
     </div>
   </div>
