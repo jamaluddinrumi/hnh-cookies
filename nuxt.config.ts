30e788aa60d05400d424fdc748bc99582807eff6
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-04-05 02:59:07 +0700

    chore: delete unused configs in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  vite: {
-    build: {
-      target: "esnext",
-    },
-  },
-
   nitro: {
     esbuild: {
       options: {
         target: "esnext",
       },
-    },
-  },
-
-  runtimeConfig: {
-    // Private config that is only available on the server
-    apiSecret: "123",
-    // Config within public will be also exposed to the client
-    public: {
-      waNo: "",
-      waUrl: "",
     },
   },
 
